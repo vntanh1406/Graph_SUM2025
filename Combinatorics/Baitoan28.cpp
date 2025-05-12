35f0d1c9d200d71a4815af9c2360fc630973f87d
noname180294 <phuctuan2103@gmail.com>
2025-05-12 21:09:09 +0700

    Bai toan 28: Chung minh \sum_{i=1}^n i C_n^i = n 2^{n-1}

---
+++ Combinatorics/Baitoan28.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+long long nCr(int n, int r) {
+    if (r>n-r) r=n-r;
+    long long res=1;
+    for(int i=1; i<=r; i++) {
+        res*=(n-r+i);
+        res/=i;
+    }
+    return res;
+}
+
+long long veTrai(int n) {
+    long long res=0;
+    for(int i=1; i<=n; i++) {
+        res+=i*nCr(n,i);
+    }
+    return res;
+}
+
+long long vePhai(int n) {
+    return (n*(1<<(n-1)));
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    int n; cin>>n;
+    cout<<"Ve trai = " << veTrai(n) << '\n';
+    cout<<"Ve phai = " << vePhai(n);
+}
